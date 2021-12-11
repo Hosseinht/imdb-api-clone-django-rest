47d65c740867ef87b4f67fa2799df0624a09b92f
Ryan Taheri <sailantht@gmail.com>
2021-12-11 10:16:57 +0330

    Serializer Relations

--- movielist/api/urls.py
+++ movielist/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>/', WatchlistDetailAV.as_view(), name='movie-detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
-    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='movie-detail'),
+    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream-detail'),
 ]

103f6a3946ac53372af79b6cb62d4f808226fdb3
Ryan Taheri <sailantht@gmail.com>
2021-12-11 14:57:00 +0330

    URL Structure

--- movielist/api/urls.py
+++ movielist/api/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('<int:pk>/', WatchlistDetailAV.as_view(), name='movie-detail'),
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream-detail'),
-    path('review/', ReviewList.as_view(), name='review-list'),
-    path('review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
+    path('stream/<int:pk>/review/', ReviewList.as_view(), name='review-list'),
+    path('stream/review/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
 ]

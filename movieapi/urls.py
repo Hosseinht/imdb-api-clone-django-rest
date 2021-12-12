8bedea2024da459c6231197b2f6a378a74504940
Ryan Taheri <sailantht@gmail.com>
2021-12-12 15:18:39 +0330

    Temporary Login & Logout

--- movieapi/urls.py
+++ movieapi/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('movielist.api.urls', namespace='watchlist')),
+    path('api-auth/', include('rest_framework.urls')),
 
 ]
